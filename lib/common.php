5fe37b69d2355c450fcf4495d37a92cdcb3bd545
Kamran Sarwar <kamransarwar47@gmail.com>
2021-08-20 18:31:51 +0500

    Fixed Database export issue
    Fixed Database export issue

--- lib/common.php
+++ lib/common.php
@@ -1,4 +1,15 @@
 <?
+    function pre($data, $json = false, $die = false) {
+        echo '<pre>';
+        if($json) {
+            $data = json_encode($data);
+        }
+        print_r($data);
+        echo '</pre>';
+        if($die) {
+            die();
+        }
+    }
     function setMessage($message, $success = false)
     {
         if ($success) {
@@ -1423,62 +1434,30 @@
      */
     function Export_Database($host, $user, $pass, $name, $tables = false, $backup_name = false)
     {
-        $mysqli = new mysqli($host, $user, $pass, $name);
-        $mysqli->select_db($name);
-        $mysqli->query("SET NAMES 'utf8'");
-
-        $queryTables = $mysqli->query('SHOW TABLES');
-        while ($row = $queryTables->fetch_row()) {
-            $target_tables[] = $row[0];
+        include_once(dirname(__FILE__) . '/Mysqldump/Mysqldump.php');
+        try {
+            $dump = new Ifsnop\Mysqldump\Mysqldump('mysql:host='.$host.';dbname='.$name, $user, $pass);
+            $dump->start($backup_name);
+        } catch (\Exception $e) {
+            echo 'mysqldump-php error: ' . $e->getMessage();
         }
-        if ($tables !== false) {
-            $target_tables = array_intersect($target_tables, $tables);
+        // Process file download
+        if(file_exists($backup_name)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="'.basename($backup_name).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($backup_name));
+            flush(); // Flush system output buffer
+            readfile($backup_name);
+            unlink($backup_name);
+            die();
+        } else {
+            http_response_code(404);
+            die();
         }
-        foreach ($target_tables as $table) {
-            $result        = $mysqli->query('SELECT * FROM ' . $table);
-            $fields_amount = $result->field_count;
-            $rows_num      = $mysqli->affected_rows;
-            $res           = $mysqli->query('SHOW CREATE TABLE ' . $table);
-            $TableMLine    = $res->fetch_row();
-            $content       = (!isset($content) ? '' : $content) . "\n\n" . $TableMLine[1] . ";\n\n";
-
-            for ($i = 0, $st_counter = 0; $i < $fields_amount; $i++, $st_counter = 0) {
-                while ($row = $result->fetch_row()) { //when started (and every after 100 command cycle):
-                    if ($st_counter % 100 == 0 || $st_counter == 0) {
-                        $content .= "\nINSERT INTO " . $table . " VALUES";
-                    }
-                    $content .= "\n(";
-                    for ($j = 0; $j < $fields_amount; $j++) {
-                        $row[$j] = str_replace("\n", "\\n", addslashes($row[$j]));
-                        if (isset($row[$j])) {
-                            $content .= '"' . $row[$j] . '"';
-                        } else {
-                            $content .= '""';
-                        }
-                        if ($j < ($fields_amount - 1)) {
-                            $content .= ',';
-                        }
-                    }
-                    $content .= ")";
-                    //every after 100 command cycle [or at last line] ....p.s. but should be inserted 1 cycle eariler
-                    if ((($st_counter + 1) % 100 == 0 && $st_counter != 0) || $st_counter + 1 == $rows_num) {
-                        $content .= ";";
-                    } else {
-                        $content .= ",";
-                    }
-                    $st_counter = $st_counter + 1;
-                }
-            }
-            $content .= "\n\n\n";
-        }
-        //$backup_name = $backup_name ? $backup_name : $name."___(".date('H-i-s')."_".date('d-m-Y').")__rand".rand(1,11111111).".sql";
-        $date        = date("Y-m-d");
-        $backup_name = $backup_name ? $backup_name : $name . ".$date.sql";
-        header('Content-Type: application/octet-stream');
-        header("Content-Transfer-Encoding: Binary");
-        header("Content-disposition: attachment; filename=\"" . $backup_name . "\"");
-        echo $content;
-        exit;
     }
 
     /* KAMRAN UDPATE ENDS */
